ccb16efaf1c5d632af4d4bb714be51753cd0f2cd
AKASH BHUJABAL <akashbhujabaliter@gmail.com>
2025-02-25 20:01:10 +0530

    Add files via upload
    Q1 updated

---
+++ Q1.java
@@ -0,0 +1,17 @@
+package patterns_Java;
+
+public class Q1 {
+    public static void main(String[] args) 
+    {
+        for (int i=1;i<=4;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+
+}
