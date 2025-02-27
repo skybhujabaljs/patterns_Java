b7655d5f34acd181239396abaf72cf37d993c8c1
AKASH BHUJABAL <akashbhujabaliter@gmail.com>
2025-02-27 15:04:06 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -7,10 +7,17 @@ public class Q2 {
         {
             for (int j=1;j<=i;j++)
             {
-                System.err.print(i);
+                System.out.print(i+" ");
             }
             System.out.println();
         }
     }
 
 }
+
+
+Output:-
+    1
+    2 2
+    3 3 3
+    4 4 4 4
